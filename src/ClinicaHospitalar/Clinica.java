47b05fcb15298a2b9e3640db2300b2d3451dc4b6
Savio <saviocarvalho@alu.ufc.br>
2023-12-04 21:17:02 -0300

    edition clinica

--- src/ClinicaHospitalar/Clinica.java
+++ src/ClinicaHospitalar/Clinica.java
@@ -1,4 +1,3 @@
-
 package ClinicaHospitalar;
 
 import java.sql.SQLException;
@@ -7,7 +6,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Clinica {
-
     private List<Paciente> pacientes = new ArrayList<>();
     private List<Medico> medicos = new ArrayList<>();
 
@@ -16,6 +14,10 @@ public class Clinica {
     public Clinica() throws SQLException {
     }
 
+    // ADICIONAR PESSOA
+    // se pessoa for instanciado de Paciente, chama o adicionarPaciente que esta no
+    // sql e retorna a funcao
+    // se nao, chama o adicionarMedico passando os parametros
     public void addPessoa(Pessoa pessoa) throws Exception {
         if (pessoa instanceof Paciente) {
             sql.adicionarPaciente(
@@ -26,11 +28,14 @@ public class Clinica {
         }
 
         sql.adicionarMedico(
-                 ((Medico) pessoa).nome,
+                ((Medico) pessoa).nome,
                 ((Medico) pessoa).telefone,
                 ((Medico) pessoa).especializacao);
     }
 
+    // ADICIONAR CONSULTA
+    // se consulta for instanciado de Particular, adiciona uma consulta,
+    // e adiciona tbm no paciente e no medico, passando os parametros
     public void addConsulta(Consulta consulta) throws SQLException {
         if (consulta instanceof Particular) {
             sql.adicionarConsulta(
@@ -41,8 +46,7 @@ public class Clinica {
                     consulta.diagnostico,
                     consulta.data,
                     "Particular",
-                    ((Particular) consulta).valor
-            );
+                    ((Particular) consulta).valor);
             sql.addConsultaNoPaciente(consulta.paciente.numeroRegistroPaciente);
             sql.addConsultaNoMedico(consulta.medico.nome);
             return;
@@ -56,12 +60,15 @@ public class Clinica {
                 consulta.diagnostico,
                 consulta.data,
                 "Plano(" + ((Plano) consulta).nomePlano + ")",
-                0
-        );
+                0);
         sql.addConsultaNoPaciente(consulta.paciente.numeroRegistroPaciente);
         sql.addConsultaNoMedico(consulta.medico.nome);
     }
 
+    // ADICIONAR PACIENTE
+    // percorro a lista de pacientes, se o paciente for igual ao paciente passado
+    // como parametro,
+    // retorna a funcao passando o paciente encontrado
     public Paciente existePac(int numRegPac, String nomePac) {
         for (Paciente pac : pacientes) {
             if (pac.nome.equals(nomePac) && pac.numeroRegistroPaciente == numRegPac) {
@@ -71,6 +78,10 @@ public class Clinica {
         return null;
     }
 
+    // ADICIONAR MEDICO
+    // percorro a lista de medicos, se o medico for igual ao medico passado como
+    // parametro,
+    // retorna a funcao passando o medico encontrado
     public Medico existeMed(String nomeMed) {
         for (Medico med : medicos) {
             if (med.nome.equals(nomeMed)) {
@@ -80,19 +91,23 @@ public class Clinica {
         return null;
     }
 
-    String getMedicosPorEspecialidade(Label especialidade) {
+    // pega o medico pela especialidade que eh passada como parametro,
+    // retornando o nome do medico encontrado
+    public String getMedicosPorEspecialidade(Label especialidade) {
         String saida = "";
         for (Medico med : medicos) {
             if (med.especializacao.equals(especialidade)) {
                 saida += "[" + med.nome + "]";
             }
         }
-        if(saida.equals("")){
+        if (saida.equals("")) {
             return "Vazio";
         }
         return saida;
     }
 
+    // informacoes do paciente, retornando a informacao do paciente encontrado
+    // se nao, retorna uma excecao
     public String infoPaciente(int numRegPac, String nome) throws Exception {
         for (Paciente pac : pacientes) {
             if (nome.equals(pac.nome) && numRegPac == pac.numeroRegistroPaciente) {
@@ -102,6 +117,8 @@ public class Clinica {
         throw new Exception("Paciente Inexistente");
     }
 
+    // informacoes do medico, retornando a informacao do medico encontrado
+    // se nao, retorna uma excecao
     public String infoMedico(String nome) throws Exception {
         for (Medico med : medicos) {
             if (nome.equals(med.nome)) {
@@ -111,6 +128,7 @@ public class Clinica {
         throw new Exception("Medico Inexistente");
     }
 
+    // atualiza a lista de pacientes e medicos
     public void update() throws Exception {
         try {
             pacientes = new ArrayList<>();
@@ -118,10 +136,16 @@ public class Clinica {
             medicos = new ArrayList<>();
             medicos = new ArrayList<>((List<Medico>) sql.updateListMed((ArrayList<Medico>) medicos));
         } catch (SQLException ex) {
-            Logger.getLogger(Clinica.class.getName()).log(Level.SEVERE, null, ex);
+            // Logger.getLogger(Clinica.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
+    // REMOVER PACIENTE
+    // se o paciente existir, remove o paciente
+    // se nao, retorna uma excecao
+    // se o paciente tiver consulta, pergunta se deseja excluir a consulta e remove
+    // a consulta
     public void removerPaciente(int idPac, String nomePac) throws Exception {
         Paciente pac = existePac(idPac, nomePac);
         if (pac == null) {
@@ -138,7 +162,11 @@ public class Clinica {
         int idpac = idPac;
         sql.removerPaciente(idpac, nomePac);
     }
-    
+
+    // REMOVER MEDICO
+    // se o medico existir, remove o medico
+    // se nao, retorna uma excecao
+    // se o medico tiver consulta, avise-o que ele tem consulta existente e pegue o nome do paciente
     public void removerMedico(String nomeMed) throws Exception {
         Medico med = existeMed(nomeMed);
         if (med == null) {
@@ -148,9 +176,9 @@ public class Clinica {
         if (med.consulta != null) {
             ClinicaHospitalar.println(nomeMed.toUpperCase() + " tem consulta existente, avise aos seguintes pacientes: ");
             ClinicaHospitalar.print(sql.getNomeDoPacienteNaConsultaDoMedico(nomeMed));
-        }        
+        }
     }
-    
+
     @Override
     public String toString() {
         String saida = "";
